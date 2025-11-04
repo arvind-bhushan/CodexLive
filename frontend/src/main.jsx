fcc8ec107ce03f90f4bc839bf7c21c218337dc31
Arvind-bhushan <arvindbhushan001@gmail.com>
2025-11-04 12:36:45 +0530

    Fixed some bugs and make better UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,7 @@ import { ClerkProvider } from "@clerk/clerk-react";
 import { BrowserRouter } from "react-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-// Import your Publishable Key
+
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 if (!PUBLISHABLE_KEY) {
